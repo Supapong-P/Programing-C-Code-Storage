57fda95f44d6658b9ce1ee75c0d6f264177d4af7
Supapong-P <supapong.pasmanee@gmail.com>
2025-07-09 11:25:50 +0700

    Create 5 data types.cpp

---
+++ 5 data types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int i = 14;
+bool t = true;
+float f = 3.14;
+double d = 0.45;
+char c = 'a';
+
+int main ()
+{
+cout <<"int type = "<< i << '\n'; //declares int data type
+cout <<"bool type = "<< boolalpha << t << '\n'; //declares bool type
+cout <<"float type = " << f << '\n'; //declares float type
+cout <<"double type = "<< d <<'\n'; //declares double type
+cout <<"char type = "<< c << '\n'; //declares char type
+return 0;
+}
