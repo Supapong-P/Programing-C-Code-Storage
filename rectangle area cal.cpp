382eafe2c19c700154ab9f9cddf568957f07f2ab
Supapong-P <supapong.pasmanee@gmail.com>
2025-07-09 11:25:21 +0700

    Create rectangle area cal.cpp

---
+++ rectangle area cal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+int l = 15;
+int w = 10;
+
+cout << "Length is = " << l << '\n';
+cout << "Width is = " << w << '\n';
+cout << "The area of this rectangle is = " << l*w <<'\n';
+return 0;
+}
