48934a13dba043434d0f37920e3be1503b01c4d2
Supapong-P <supapong.pasmanee@gmail.com>
2025-07-29 11:11:58 +0700

    Create Reverse Sentence.cpp

---
+++ Reverse Sentence.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string mess = "Quick brown fox";
+
+    for(int i=14;i>=0;i--)
+    {
+        cout << mess[i];
+        
+    }
+}
